07ad8fb9588dbac8e05dbc958914370c7f4d2a54
Mr.Woo <479345212@qq.com>
2017-06-29 21:57:13 +0800

    sport and friends-1.0.0

---
+++ app/data.js
@@ -0,0 +1,152 @@
+const data = {
+  "calendar": [
+    {
+      "weekday": "Monday",
+      "title": "Art in Full Color",
+      "description": "The first day of CAC events and exhibits is kicked off under the theme of Art in Full Color From a demonstration in graffiti art on a wall of the Rousseau Room, to the exhibit of colorful glazed modern glassware in the Dover Hall, Art in Full Color will get CAC started in full swing!",
+      "schedule": [
+        {
+          "time": "9:30-10:30am",
+          "shortname": "LaVonne_LaRue",
+          "artist": "LaVonne L. LaRue",
+          "room": "Elizabeth Hall",
+          "description": "Watch LaVonne L. LaRue, a Chicago graffiti artist share her love and skill of mural art on Monday's schedule, as she starts the painting of a 20-foot high wall in the Rousseau Room of Hotel Contempo, which will be finished at the end of the conference. Make sure to show up a bit early, as this session will be standing-room only.",
+          "title": "Art in Unexpected Places"
+        }, {
+          "time": "11:00am-1pm",
+          "shortname": "Hassum_Harrod",
+          "name": "Hassum Harrod",
+          "room": "Victoria Hall",
+          "description": "Drawing and painting flowers may seem like a first-year art student's assignment, but Hassum Harrod brings depth, shadows, light, and color to new heights with his unique technique of painting on canvas with ceramic glaze. This session is sure to be a hit with mixed media buffs.",
+          "title": "Art in Full Bloom"
+        },{
+          "time": "2:30-4:00pm",
+          "room": "Dennison Hall",
+          "description": "Grab your pencils, charcoal, acrylics, watercolors, or whatever painting tools suit your fancy, and participate in the capturing of various still life settings that are staged all around Dennison Hall. You won't believe the wealth and depth of choices.",
+          "title": "Still Life"
+        }
+      ]
+    }, {
+      "weekday": "Tuesday",
+      "title": "Water in Art",
+      "description": "Water in Art is the theme for the second day, as art students from around the world gather at the Fountain of Intrigue in the gardens of Hotel Contempo to create ice sculptures, and art lecturers discuss the use of water as an art material, and water as an art subject.",
+      "schedule": [
+        {
+          "time": "9:30-10:30am",
+          "shortname": "Jennifer_Jerome",
+          "artist": "Jennifer Jerome",
+          "room": "Elizabeth Hall",
+          "description": "Jennifer Jerome, a native of New Orleans, whose work has centered around abstract images that depict flooding and rebuilding, will talk about how the floods inspired her artistically, and how, despite the sadness of devastation and lives lost, her work also depicts the hope and togetherness of a community that has persevered.",
+          "title": "Water in Art Kickoff Session"
+        },{
+          "time": "10:30am-1pm",
+          "room": "Fountain of Intrigue",
+          "description": "Get on your mittens and earmuffs, and join your fellow artists at the Fountain of Intrigue, in the Hotel Contempo gardens, where the ambient temperature has been turned down to allow the sculpting of ice into the most mysterious and beautiful of shapes. Various masters will share their secrets for chiseling ice into a shape that their imagination has envisioned.",
+          "title": "Ice Sculptures"
+        },{
+          "time": "2:30-4:00pm",
+          "shortname": "Hillary_Goldwynn",
+          "artist": "Hillary Hewitt Goldwynn-Post",
+          "room": "Dennison Hall",
+          "description": "Hilary Goldywynn Post has been inspiring deep sea divers to paint what they experience under water, since she began diving at the early age of 14. Not only does she explain texture, color, and tools, but she also explains methods for capturing your under sea explorations in your mind for future expulsion onto canvas. And, even if snorkeling is as far under water as you're willing to go, Hilary's techniques will work just as well for you.",
+          "title": "Deep Sea Wonders"
+        }
+      ]
+    }, {
+      "weekday": "Wednesday",
+      "schedule": [
+        {
+          "time": "9:30 to 10:30 am",
+          "shortname": "Barot_Bellingham",
+          "artist": "Barot Bellingham",
+          "room": "Elizabeth Hall",
+          "description": "Barot Bellingham's new collection is pieced together from seemingly unrelated individual works of varying topic. From portraits to landscapes to abstract, Barot does it all, and does it all well. The only unifying factor is the excellence in form, technique, color, and balance that exudes from each and every individual work in The Un-Collection. Meet Barot, and his undeniably original collection of art, and be forever enchanted.",
+          "title": "The Un-Collection"
+        },{
+          "time": "10:45 to 12:00 pm",
+          "shortname": "Xhou_Ta",
+          "artist": "Xhou Ta",
+          "room": "Dennison Hall",
+          "description": "Get a glimpse at the rare offerings in Xhou's exhibit, ranging from an exquisite skyscraper painted on a grain of rice, to a miniature sculpture of an oak tree filled with detailed animals, the size of a thimble. Items are shown on a large screen, using Xhou's patented rear-projection device that magnifies the image to reveal incredible detail. Beyond the Naked Eye is a fantastic exhibit that blends art and science in a most magical way.",
+          "title": "Beyond the Naked Eye"
+        }
+      ]
+    }, {
+      "weekday": "Thursday",
+      "schedule": [
+        {
+          "time": "09:30 to 10:30 am",
+          "shortname": "Hassum_Harrod",
+          "artist": "Hassum Harrod",
+          "room": "Elizabeth Hall",
+          "description": "Hassum's inherent ability to create color palettes that illuminate an image and allure an audience, has drawn praise from traditionalists as well as modern artists - no small feat in today's diverse art world. His master's thesis at San Francisco Art Academy is entitled \"Color Upon Color â€“ The Mixing and Reinventing of Color.\" It is this thesis that prompted Hillford & Bilken to publish his solo-authored book on color, that will be used in undergraduate art programs throughout the world starting next year.",
+          "title": "The Use and Absence of Color in Modern Art"
+        },{
+          "time": "10:30 to 11:30 am",
+          "shortname": "Constance_Smith",
+          "artist": "Constance Olivia Smith",
+          "room": "Victoria Hall",
+          "description": "In the Art of Salvaging, Olivia will share her talent for finding and selecting scrap materials, as well as the philosophy behind several of her more celebrated works of art. If you're interested in recycling and mixed media, this session will include a lively discussion of both, from the woman who Art International Digest calls the \"foremost queen of scrap art.\"",
+          "title": "The Art of Salvaging"
+        },{
+          "time": "1:30 to 2:30",
+          "shortname": "Riley_Rewington",
+          "artist": "Riley Rudolph Rewington",
+          "room": "Dennison Hall",
+          "description": "The leader of the MMA artistic movement in his hometown of Portland, Riley Rudolph Rewington draws a crowd wherever he goes. Mixing street performance, video, music, and traditional art, Riley has created some of the most unique and deeply poignant abstract works of his generation. Join Riley in Dennison Hall for an intimate discussion on The Art of Abstract, in which he shares his philosophies about modern abstract art, and critiques some of this own work with complete candor and a healthy dose of personality.",
+          "title": "The Art of Abstract"
+        }
+      ]
+    }
+  ],
+  "artists": [
+    {
+      "bio": "从很小时候一直对滑板有者由衷的热爱.形式的多样化使滑板与众不同，来自世界各地的爱好者们聚集在一起并成为朋友。很多人认为我们是与吸毒什么的有点关系，事实上只有很小一部分人是这样，其实滑板运动是需要很好的身体素质的，那就是滑板的与众不同处。",
+      "shortname": "Barot_Bellingham",
+      "name": "文森森",
+      "reknown": "一个热爱滑板的女孩"
+    },{
+      "bio": "探险可谓是众多热血男儿所热爱的项目，在森林中探索，在悬崖陡壁之间上下攀爬，如履平地，在深不见底的深渊之上攀过，这种种一想起来就令人热血沸腾，浑身好似充满了力量。",
+      "shortname": "Jonathan_Ferrar",
+      "name": "J_大强",
+      "reknown": "探险爱好者"
+    },{
+      "bio": "The action of parkour is combining with all the free actions, enhance creativity and encourage people to find their own beliefs. In the field of parkour, many Traceur tried to express flips.spin, these are not included by the action, which also expressed parkour is not limited to some fixed action. You can also get even said your action to Parkour.",
+      "shortname": "Hillary_Goldwynn",
+      "name": "Hillary Hewitt Goldwynn-Post",
+      "reknown": "Is the heart of the play, more daring to be cautious"
+    },{
+      "bio": "我过去的兴趣一直在如何保持完美的身体比例上。有一次，15岁的我脱光衣服站在镜子前看自己。看着镜中裸体的自己，我发现要达到完美的身体比例，我得有一对20英寸粗的臂膀，才配得上身体的其它地方。",
+      "shortname": "Hassum_Harrod",
+      "name": "张_小小",
+      "reknown": "健身让人更美丽动人"
+    },{
+      "bio": "在每一丝曙光破晓前，一定是快要窒息的漫长黑夜;在每一次荣光到来前，一定有太多狼狈的时刻，被看不起的日子;在每一阵掌声到来前，总有太多唏嘘，太多冷眼;在每一个山顶峰巅，总有贝壳;因此，在每一个快要放弃的时刻，记得对自己说：加油，挺住!",
+      "shortname": "Jennifer_Jerome",
+      "name": "Jennifer Jerome",
+      "reknown": "New Orleans, LA"
+    },{
+      "bio": "每一个优秀的人，都有一段沉默的时光。那一段时光，是付出了很多努力，忍受孤独和寂寞，不抱怨不诉苦，日后说起时，连自己都能被感动的日子。",
+      "shortname": "LaVonne_LaRue",
+      "name": "LaVonne L. LaRue",
+      "reknown": "Chicago, IL"
+    },{
+      "bio": "The failures and reverses which await men - and one after another sadden the brow of youth - add a dignity to the prospect of human life, which no Arcadian success would do.",
+      "shortname": "Constance_Smith",
+      "name": "Constance Olivia Smith",
+      "reknown": "Just Do It"
+    },{
+      "bio": "A first-year student at the Roux Academy of Art, Media, and Design, Riley is already changing the face of modern art at the university. Riley's exquisite abstract pieces have no intention of ever being understood, but instead beg the viewer to dream, create, pretend, and envision with their mind's eye. Riley will be speaking on the 'Art of Abstract' during Thursday's schedule",
+      "shortname": "Riley_Rewington",
+      "name": "Riley Rudolph Rewington",
+      "reknown": "Roux Academy of Art, Media, and Design"
+    },{
+      "bio": "跑步的人并不是在跟别人比赛，是跟自己的挑战。你可以发现你每天都在破自己的纪录，也许更快，也许更远。心理学家认为在跑步时抛开繁琐的人际关系而独处一段时间是有益心理健康的。运动心理的研究者更发现跑步的人能因每天完成自己的目标而得到成就感，累计这些成就感会让跑者更显得有自信心。",
+      "shortname": "Xhou_Ta",
+      "name": "Xhou Ta",
+      "reknown": "Running And Running"
+    }
+  ]
+}
+
+export default data
---
+++ app/imgs/ImageSources.js
@@ -0,0 +1,15 @@
+export const avatars = {
+  'Riley_Rewington': require('./Riley_Rewington_tn.jpg'),
+  'LaVonne_LaRue': require('./6.jpg'),
+  'Jonathan_Ferrar': require('./2.jpg'),
+  'Jennifer_Jerome': require('./5.jpg'),
+  'Hillary_Goldwynn': require('./3.jpg'),
+  'Hassum_Harrod': require('./4.jpg'),
+  'Constance_Smith': require('./7.jpeg'),
+  'Barot_Bellingham': require('./1.jpg'),
+  'Xhou_Ta': require('./8.jpeg')
+}
+
+export const homeGraph = require('./bg-index.jpg');
+
+// export const svgLogo = require('./rouxacademylogo.svg');
